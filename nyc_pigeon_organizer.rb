4a867bd9caf067196ed2a3abd8d21c98940e674b
yash patel <yash82099@yahoo.com>
2019-11-07 22:51:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |trait, attributes|
+    attributes.each do |details, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][trait] ||= []
+        pigeon_list[name][trait] << details.to_s
+      end
+    end
+  end
+  return pigeon_list
 end
