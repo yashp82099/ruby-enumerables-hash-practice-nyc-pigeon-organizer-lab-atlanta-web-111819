c2b05784c5386f1074be95f73e0cf4acb45fb031
yash patel <yash82099@yahoo.com>
2019-11-07 22:55:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = {}
+  data.each do |trait, attributes|
+    attributes.each do |details, names|
+      names.each do |name|
+        list[name] ||= {}
+        list[name][trait] ||= []
+        list[name][trait] << details.to_s
+      end
+    end
+  end
+  return list
 end
